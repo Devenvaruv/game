222994495f008ce783d8630466b5be6a9d302b28
Devenvaruv <devenvaruv@gmail.com>
2022-06-11 14:59:11 -0700

    p tag

--- src/components/titleScreen/TitleScreen.js
+++ src/components/titleScreen/TitleScreen.js
@@ -4,8 +4,9 @@ class TitleScreen extends React.Component {
     render() {
         return (
             <div>
-                <p>{`This will be a titlescreen which will greet people and then move forward to the game choice and selections`}</p>
-                <img src="https://toppng.com/uploads/preview/fire-logo-png-svg-free-download-fire-logo-11563553513c3wo0p7dt1.png" alt="" />
+                <p>{`This will be a title screen which will greet people and then move forward to the game choice and selections`}</p>
+                <p>11563553513c3wo0p7dt1</p>
+                <img src="https://toppng.com/uploads/preview/fire-logo-png-svg-free-download-fire-logo-11563553513c3wo0p7dt1.png" alt="fire logo" />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>this is index.js</h1>
     <App />
   </React.StrictMode>
 );
