6e3a9442e076b825c44a067407ccb6adf2289e54
Devenvaruv <devenvaruv@gmail.com>
2022-06-29 12:54:21 -0700

    f

--- src/components/titleScreen/TitleScreen.js
+++ src/components/titleScreen/TitleScreen.js
@@ -7,6 +7,7 @@ class TitleScreen extends React.Component {
                 <p>{`This will be a title screen which will greet people and then move forward to the game choice and selections`}</p>
                 
                 <p>{` new photos to uploads`}</p>
+                <p>{` transact to new screen`}</p>
                 
                 
                 <img src="https://toppng.com/uploads/preview/fire-logo-png-svg-free-download-fire-logo-11563553513c3wo0p7dt1.png" alt="fire logo" />
